db2d47f0dba6ad65034364480cdf9d7a4a219f6c
Abhishek Gupta <abhishekgupta311287@gmail.com>
2020-11-11 12:24:06 +0530

    Changes in GithubApi to use new data classes

--- app/src/main/java/com/abhishekgupta/githubsearch/repo/network/GithubApi.kt
+++ app/src/main/java/com/abhishekgupta/githubsearch/repo/network/GithubApi.kt
@@ -1,6 +1,7 @@
 package com.abhishekgupta.githubsearch.repo.network
 
-import com.abhishekgupta.githubsearch.model.Follow
+import com.abhishekgupta.githubsearch.model.Follower
+import com.abhishekgupta.githubsearch.model.Following
 import com.abhishekgupta.githubsearch.model.User
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -9,20 +10,20 @@ import retrofit2.http.Query
 interface GithubApi {
 
     @GET("/users/{user}")
-    suspend fun getUser(@Path("user") user: String): User
+    suspend fun getUser(@Path("user") user: String): User?
 
     @GET("users/{user}/followers")
     suspend fun getUserFollowers(
         @Path("user") user: String,
         @Query("per_page") limit: Int,
         @Query("page") page: Int
-    ): List<Follow>
+    ): List<Follower>
 
     @GET("users/{user}/following")
     suspend fun getUserFollowing(
         @Path("user") user: String,
         @Query("per_page") limit: Int,
         @Query("page") page: Int
-    ): List<Follow>
+    ): List<Following>
 
 }
