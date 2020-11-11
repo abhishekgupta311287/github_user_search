6486ba6b4189d142fd1aab6a4845a8c3d4238a9a
Abhishek Gupta <abhishekgupta311287@gmail.com>
2020-11-11 12:32:32 +0530

    Added SearchViewModel implementation

---
+++ app/src/main/java/com/abhishekgupta/githubsearch/di/AppModule.kt
@@ -0,0 +1,12 @@
+package com.abhishekgupta.githubsearch.di
+
+import com.abhishekgupta.githubsearch.repo.SearchRepository
+import com.abhishekgupta.githubsearch.repo.SearchRepositoryImpl
+import com.abhishekgupta.githubsearch.view.SearchViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val appModule = module {
+    single<SearchRepository> { SearchRepositoryImpl(get(), get()) }
+    viewModel { SearchViewModel(get()) }
+}
--- app/src/main/java/com/abhishekgupta/githubsearch/view/SearchViewModel.kt
+++ app/src/main/java/com/abhishekgupta/githubsearch/view/SearchViewModel.kt
@@ -1,8 +1,69 @@
 package com.abhishekgupta.githubsearch.view
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.abhishekgupta.githubsearch.model.Follower
+import com.abhishekgupta.githubsearch.model.Following
+import com.abhishekgupta.githubsearch.model.Resource
+import com.abhishekgupta.githubsearch.model.UserDetail
+import com.abhishekgupta.githubsearch.repo.SearchRepository
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 
-class SearchViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class SearchViewModel(
+    private val repo: SearchRepository
+) : ViewModel() {
+
+    val userLiveData: MutableLiveData<Resource<UserDetail>> = MutableLiveData()
+    val followerLiveData: MutableLiveData<Resource<List<Follower>>> = MutableLiveData()
+    val followingLiveData: MutableLiveData<Resource<List<Following>>> = MutableLiveData()
+
+    fun getUserDetails(userName: String) {
+        viewModelScope.launch {
+
+            userLiveData.value = Resource.Loading()
+
+            val user = async { repo.getUser(userName) }.await()
+
+            val followers = async { repo.getUserFollowers(userName) }.await()
+
+            val following = async { repo.getUserFollowing(userName) }.await()
+
+            if (user != null) {
+                userLiveData.value = Resource.Success(
+                    UserDetail(user, followers, following)
+                )
+            } else {
+                userLiveData.value = Resource.Error("Unable to fetch user details")
+            }
+
+        }
+    }
+
+
+    suspend fun getUserFollowers(userName: String) {
+        followerLiveData.value = Resource.Loading()
+        val followers = repo.getUserFollowers(userName)
+
+        if (followers.isEmpty()) {
+            followerLiveData.value = Resource.Error("No more followers")
+        } else {
+            followerLiveData.value = Resource.Success(followers)
+        }
+    }
+
+    suspend fun getUserFollowing(userName: String) {
+        followingLiveData.value = Resource.Loading()
+
+        val following = repo.getUserFollowing(userName)
+
+        if (following.isEmpty()) {
+            followingLiveData.value = Resource.Error("No more following")
+        } else {
+            followingLiveData.value = Resource.Success(following)
+        }
+    }
+
 }
