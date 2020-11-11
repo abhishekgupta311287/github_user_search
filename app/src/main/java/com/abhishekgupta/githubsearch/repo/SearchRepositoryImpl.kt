65d301c75c6e00111799740802543cf583aba4a4
Abhishek Gupta <abhishekgupta311287@gmail.com>
2020-11-11 17:08:14 +0530

    Reset page count when user is searched again

--- app/src/main/java/com/abhishekgupta/githubsearch/repo/SearchRepositoryImpl.kt
+++ app/src/main/java/com/abhishekgupta/githubsearch/repo/SearchRepositoryImpl.kt
@@ -11,11 +11,14 @@ class SearchRepositoryImpl(
     private val dao: SearchDao
 ) : SearchRepository {
 
-    var followerPage = 1
-    var followingPage = 1
+    private var followerPage = 1
+    private var followingPage = 1
 
     override suspend fun getUser(userName: String): User? {
         return try {
+            // reset page count when user is searched
+            followerPage = 1
+            followingPage = 1
             var user = dao.getUser(userName)
 
             if (user == null) {
@@ -32,38 +35,46 @@ class SearchRepositoryImpl(
     }
 
     override suspend fun getUserFollowers(userName: String): List<Follower> {
-        val start = LIMIT * (followerPage - 1)
+        return try {
+            val start = LIMIT * (followerPage - 1)
 
-        var followers = dao.getUserFollowers(userName, LIMIT, start)
+            var followers = dao.getUserFollowers(userName, LIMIT, start)
 
-        if (followers.isEmpty()) {
-            followers = api.getUserFollowers(userName, LIMIT, followerPage)
+            if (followers.isEmpty()) {
+                followers = api.getUserFollowers(userName, LIMIT, followerPage)
 
-            if (followers.isNotEmpty()) {
-                dao.insertUserFollowers(userName, followers)
+                if (followers.isNotEmpty()) {
+                    dao.insertUserFollowers(userName, followers)
+                }
+
             }
+            followerPage++
 
+            followers
+        } catch (e: Exception) {
+            emptyList()
         }
-        followerPage++
-
-        return followers
     }
 
     override suspend fun getUserFollowing(userName: String): List<Following> {
-        val start = LIMIT * (followingPage - 1)
+        return try {
+            val start = LIMIT * (followingPage - 1)
 
-        var following = dao.getUserFollowing(userName, LIMIT, start)
+            var following = dao.getUserFollowing(userName, LIMIT, start)
 
-        if (following.isEmpty()) {
-            following = api.getUserFollowing(userName, LIMIT, followingPage)
-            if (following.isNotEmpty()) {
-                dao.insertUserFollowing(userName, following)
+            if (following.isEmpty()) {
+                following = api.getUserFollowing(userName, LIMIT, followingPage)
+                if (following.isNotEmpty()) {
+                    dao.insertUserFollowing(userName, following)
+                }
+
             }
+            followingPage++
 
+            following
+        } catch (e: Exception) {
+            emptyList()
         }
-        followingPage++
-
-        return following
     }
 
     companion object {
